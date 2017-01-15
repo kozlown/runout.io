51da3aced8872f8e620ced4aef913ad073713a0c
Nigelk <kozlown@gmail.com>
2017-01-15 19:44:15 +0100

    added SASS loader to webpack

--- front/react-app/src/App.js
+++ front/react-app/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import './stylesheet/App.sass';
 
 class App extends Component {
   render() {
--- front/react-app/src/index.js
+++ front/react-app/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './stylesheet/css/index.css';
+import './stylesheet/index.sass';
 
 ReactDOM.render(
   <App />,
